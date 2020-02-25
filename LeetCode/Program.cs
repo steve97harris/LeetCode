460a011d08cbcc1f0c91aa32c71e028fa453a53a
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-25 20:30:17 +0000

    Initial commit

---
+++ LeetCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] strgs = {"Steve", "Steves"};
+            LongestCommonPrefix(strgs);
+        }
+        
+        static void LongestCommonPrefix(string[] strgs)
+        {
+            foreach (string word in strgs)
+            {
+                foreach (char x in word)
+                { 
+                    Console.WriteLine(x); 
+                }
+            }
+        }
+    }
+}
