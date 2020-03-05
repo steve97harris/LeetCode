320d435cbf1641c00f702bbbd2f9feabb878968d
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-03-05 09:45:11 +0000

    skdfj

--- LeetCode/Main/Solutions/MinTimeToVisitPoints.cs
+++ LeetCode/Main/Solutions/MinTimeToVisitPoints.cs
@@ -6,18 +6,42 @@ namespace LeetCode.Main.Solutions
     {
         public static int MinTimeVisitingAllPoints(int[][] points)
         {
-            int smallestValue = 0;
-            int distanceBetween2Points;
-            
+            int minimum = 0;
+            int[] current = points[0];
+
             for (int i = 0; i < points.Length; i++)
             {
-                for (int j = 0; j < points[i].Length; j++)
-                {
-                    distanceBetween2Points = points[i + 1][j + 1] - points[i][j];
-                }
+                int[] point = points[i];
+                minimum += FindDistance(current, point);
+                current = point;
             }
 
-            return smallestValue;
+            return minimum;
+        }
+
+        private static int FindDistance(int[] current, int[] point)
+        {
+            int minimum = 0;
+            int x1 = current[0];
+            int x2 = point[0];
+            int y1 = current[1];
+            int y2 = point[1];
+            
+            // Compute diagonal
+            int horizontal = Math.Abs(x2 - x1);        // Difference in horizontal values
+            int vertical = Math.Abs(y2 - y1);        // Difference in vertical values
+            int distance = Math.Min(horizontal, vertical);        // Find min value of horizontal and vertical 
+            
+            if (horizontal > vertical)
+            {
+                minimum = minimum + (horizontal - distance);
+            }
+            else
+            {
+                minimum = minimum + (vertical - distance);
+            }
+
+            return (minimum = minimum + distance);
         }
     }
 }
