52b1d0b8c5c10b3acc7eb5ec1f079c9bc1ac6eb8
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-03-24 10:20:02 +0000

    dlkf

--- LeetCode/Main/RunMe.cs
+++ LeetCode/Main/RunMe.cs
@@ -12,12 +12,16 @@ namespace LeetCode.Main
                 // string[] slkd = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
                 // SubdomainVisitCount.SubdomainVisits(slkd);
 
-                string[] emails =
-                {
-                    "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com",
-                    "testemail+david@lee.tcode.com"
-                };
-                NoOfUniqueEmails.NumUniqueEmails(emails);
+                // string[] emails =
+                // {
+                //     "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com",
+                //     "testemail+david@lee.tcode.com"
+                // };
+                // NoOfUniqueEmails.NumUniqueEmails(emails);
+
+                int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+                int[] arr2 = {2, 1, 4, 3, 9, 6};
+                RelativelySortArray.RelativeSortArray(arr1, arr2);
             }
         }
     }
--- LeetCode/Main/Solutions/RelativelySortArray.cs
+++ LeetCode/Main/Solutions/RelativelySortArray.cs
@@ -1,14 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace LeetCode.Main.Solutions
 {
     public static class RelativelySortArray
     {
         public static int[] RelativeSortArray(int[] arr1, int[] arr2)
         {
-            int[] result = new int[arr1.Length];
+            List<int> result = new List<int>();
+            List<int> leftOvers = new List<int>();
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr2[i] == arr1[j])
+                    {
+                        result.Add(arr1[j]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (!result.Contains(arr1[i]))
+                {
+                    leftOvers.Add(arr1[i]);
+                }
+            }
+
             
-            
-            
-            return result;
+
+            return arr1;
         }
     }
 }
