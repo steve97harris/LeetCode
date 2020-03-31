d691d0d9330d2b441936566cb25443120b097b51
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-03-31 22:18:56 +0100

    sdjf

--- LeetCode/Main/Solutions/FindCommonCharacters.cs
+++ LeetCode/Main/Solutions/FindCommonCharacters.cs
@@ -6,33 +6,41 @@ namespace LeetCode.Main.Solutions
 {
     public static class FindCommonCharacters
     {
-        public static IList<string> CommonChars(string[] A)
+        public static IList<string> CommonChars(string[] words)
         {
-            List<string> aList = new List<string>();
             IList<string> result = new List<string>();
 
-            for (int i = 1; i < A.Length; i++)
+            var charCount = new int[words.Length, 26];     // 2 dimensional array of characters and character count.
+            
+            for (int i = 0; i < words.Length; i++)
             {
-                aList.Add(A[i]);
-            }
+                var currentWord = words[i];
 
-            foreach (var c in A[0])
-            {
-                result.Add(c.ToString());
-
-                foreach (var word in aList)
+                foreach (var character in currentWord)
                 {
-                    if (!word.Contains(c))
-                    {
-                        result.Remove(c.ToString());
-                    }
-                    else if (word.Contains(c) && word.IndexOf(c) != word.LastIndexOf(c))
-                    {
-                        result.Remove(c.ToString());
-                    }
+                    charCount[i, character - 'a']++;        // count each char in each word and do it for all words in input strings
                 }
             }
 
+            for (int i = 0; i < 26; i++)
+            {
+                int min = charCount[0, i];
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    var current = charCount[j, i];
+                    if (current < min)
+                        min = current;
+                    else
+                        min = min;
+                }
+
+                for (int j = 0; j < min; j++)
+                {
+                    var c = (char) ('a' + i);
+                    result.Add(c.ToString());
+                }
+            }
             
             foreach (var word in result)
             {
