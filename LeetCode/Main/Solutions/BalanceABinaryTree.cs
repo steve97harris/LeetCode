7cdd8e0df9311096cdcf9d398123c196c5dff424
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-05-09 18:52:48 +0100

    aslkjd

--- LeetCode/Main/Solutions/BalanceABinaryTree.cs
+++ LeetCode/Main/Solutions/BalanceABinaryTree.cs
@@ -1,26 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode.Main
 {
     public static class BalanceABinaryTree
     {
+        private static List<int> nodeValues = new List<int>();
         public static TreeNode BalanceBST(TreeNode root)
         {
-            Nodes(root,1);
+            Nodes(root);
+            nodeValues.Sort();
+            var newRoot = BuildTree(nodeValues, 0, nodeValues.Count - 1);
 
-            return root;
+            return newRoot;
         }
 
-        private static void Nodes(TreeNode root, int level)
+        private static void Nodes(TreeNode root)
         {
             if (root == null)
                 return;
-            Console.WriteLine(root.val + ", Level: " + level);
-            
+            nodeValues.Add(root.val);
+
             if (root.left != null)
-                Nodes(root.left, level+1);
+                Nodes(root.left);
             if (root.right != null)
-                Nodes(root.right, level+1);
+                Nodes(root.right);
+        }
+
+        private static TreeNode BuildTree(List<int> nums, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            var mid = start + (end - start) / 2;
+
+            var node = new TreeNode(nums[mid])
+            {
+                left = BuildTree(nums, start, mid - 1), 
+                right = BuildTree(nums, mid + 1, end)
+            };
+            return node;
         }
     }
 }
