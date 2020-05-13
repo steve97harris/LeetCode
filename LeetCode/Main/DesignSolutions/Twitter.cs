5218a13435c6457e67e7444396f4885d8b1504dd
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-05-13 17:28:52 +0100

    laskjd

--- LeetCode/Main/DesignSolutions/Twitter.cs
+++ LeetCode/Main/DesignSolutions/Twitter.cs
@@ -1,37 +1,72 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Main
 {
     public class Twitter
     {
+        private Dictionary<int, IList<int>> userNewsFeeds;        // {userId, userNewsFeed}        
         public Twitter()
         {
-            
+            userNewsFeeds = new Dictionary<int, IList<int>>();
         }
 
         /** Compose a new tweet. */
-        public void PostTweet(int userId, int tweetId)
+        public void PostTweet(int userId, int tweetId)        // userNewsFeed - IList<int> where int is tweetId
         {
-            
+            if (userNewsFeeds.ContainsKey(userId))
+                userNewsFeeds[userId].Add(tweetId);
+            else
+                userNewsFeeds.Add(userId, new List<int>() { tweetId });
         }
         
         /** Retrieve the 10 most recent tweet ids in the user's news feed.
         Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
         public IList<int> GetNewsFeed(int userId)
         {
-            
+            var last10 = new List<int>();
+            if (!userNewsFeeds.ContainsKey(userId)) return last10;
+            last10 = userNewsFeeds[userId].TakeLast(10).ToList();
+            return last10;
+
         }
 
         /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
         public void Follow(int followerId, int followeeId)
         {
-            
+            if (!userNewsFeeds.ContainsKey(followerId) || !userNewsFeeds.ContainsKey(followeeId)) return;
+            var followeeList = userNewsFeeds[followeeId];
+            for (int i = 0; i < followeeList.Count - 1; i++)
+            {
+                userNewsFeeds[followerId].Add(followeeList[i]);
+            }
         }
 
         /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
         public void Unfollow(int followerId, int followeeId)
         {
-            
+            if (!userNewsFeeds.ContainsKey(followerId) || !userNewsFeeds.ContainsKey(followeeId)) return;
+            var followeeList = userNewsFeeds[followeeId];
+            for (int i = 0; i < followeeList.Count - 1; i++)
+            {
+                userNewsFeeds[followerId].Remove(followeeList[i]);
+            }
         }
+
+        private void DisplayAllUserNewsFeeds()
+        {
+            foreach (var pair in userNewsFeeds)
+            {
+                Console.WriteLine("UserId: " + pair.Key);
+                Console.WriteLine("NewsFeed: ");
+                foreach (var i in pair.Value)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("----");
+            }
+        }
+        
     }
 }
