4e75f63ad588a4cf6c1a491fa77266f8d5746944
steve97harris <steve97harris@hotmail.co.uk>
2020-05-16 17:52:09 +0100

    lkjd

--- LeetCode/Main/RunMe.cs
+++ LeetCode/Main/RunMe.cs
@@ -12,7 +12,7 @@ namespace LeetCode.Main
         {
             static void Main()
             {
-                
+                RotateDeArray.Rotate(new int[] {1, 2, 3, 4, 5, 6, 7}, 3);
             }        
 
             #region CodeTests
--- LeetCode/Main/Solutions/RotateDeArray.cs
+++ LeetCode/Main/Solutions/RotateDeArray.cs
@@ -1,7 +1,27 @@
-﻿namespace LeetCode.Main
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Main
 {
     public static class RotateDeArray
     {
-        
+        public static void Rotate(int[] nums, int k)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var newIndex = 0;
+                if (i + k < nums.Length)
+                    newIndex = i + k;
+                else
+                    newIndex = i;
+            }
+
+            foreach (var i in nums)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
     }
 }
