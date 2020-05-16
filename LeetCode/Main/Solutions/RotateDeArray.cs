5039d57e7448476e1a6d881536be3ae35c4d50e2
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-05-16 18:09:45 +0100

    lsdkjf

--- LeetCode/Main/Solutions/RotateDeArray.cs
+++ LeetCode/Main/Solutions/RotateDeArray.cs
@@ -8,20 +8,29 @@ namespace LeetCode.Main
     {
         public static void Rotate(int[] nums, int k)
         {
-            for (int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < k; i++)
             {
-                var newIndex = 0;
-                if (i + k < nums.Length)
-                    newIndex = i + k;
-                else
-                    newIndex = i;
+                nums = ShiftRight(nums);
             }
 
             foreach (var i in nums)
             {
                 Console.WriteLine(i);
             }
-            
+        }
+        
+        private static int[] ShiftRight(int[] arr)
+        {
+            var demo = new int[arr.Length];
+
+            for (int i = 1; i < arr.Length; i++) 
+            {
+                demo[i] = arr[i - 1];
+            }
+
+            demo[0] = arr[demo.Length - 1];
+
+            return demo;
         }
     }
 }
