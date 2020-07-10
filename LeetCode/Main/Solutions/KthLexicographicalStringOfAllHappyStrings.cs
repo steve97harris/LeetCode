2e7326e395a8fd0d7f3b7532c41ee0f5c63b2826
steve97harris <steve97harris@hotmail.co.uk>
2020-07-10 21:25:51 +0100

    ksflkassd

--- LeetCode/Main/Solutions/KthLexicographicalStringOfAllHappyStrings.cs
+++ LeetCode/Main/Solutions/KthLexicographicalStringOfAllHappyStrings.cs
@@ -1,10 +1,15 @@
-﻿namespace LeetCode.Main
+﻿using System;
+
+namespace LeetCode.Main
 {
     public class KthLexicographicalStringOfAllHappyStrings
     {
         public string GetHappyString(int n, int k)
         {
             if (n == 5)
+            {
+                Console.WriteLine("hello my name is steve");
+            }
         }
     }
 }
