aa22abc5a0ac6788fce1211ed08c668b1234d879
steve97harris <steve97harris@hotmail.co.uk>
2020-07-10 21:22:35 +0100

    nfl

---
+++ LeetCode/Main/Solutions/KthLexicographicalStringOfAllHappyStrings.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Main
+{
+    public class KthLexicographicalStringOfAllHappyStrings
+    {
+        public string GetHappyString(int n, int k)
+        {
+            if (n == 5)
+        }
+    }
+}
